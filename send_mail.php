5c97a5d9b2a92f736ddb9eb644a48a55353dfbdc
pvsrao <50945372+pvsrao@users.noreply.github.com>
2019-05-24 11:55:20 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -9,7 +9,7 @@ $webmaster_email = "sunryze@sunryze.in";
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
-$feedback_page = "feedback_form.html";
+$feedback_page = "index.html";
 $error_page = "error_message.html";
 $thankyou_page = "thank_you.html";
 
